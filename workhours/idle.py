18b2641155b0b0fc0bff85dafd1b07a7c2adccde
chopsueysensei <chopsueysensei@gmail.com>
2016-04-15 14:37:34 +0200

    + Encapsulated monitor thread in a new class + Added tooltip updater function

--- workhours/idle.py
+++ workhours/idle.py
@@ -1,20 +1,27 @@
+import os
 import ctypes
 import ctypes.wintypes
 import time
-import os
-import _thread
+from datetime import datetime, timedelta
+import threading
+import python_tools.systray_app as trayapp
 
 
-homedir = os.path.expanduser('~/.workhours')
+TIMEFORMAT = '%d/%m/%Y %H:%M:%S'
+
+homedir = os.path.expanduser('~/.workhours/')
 runfile = homedir + 'run'
+thread = None
+app = None
 
+
+#
 
 # http://msdn.microsoft.com/en-us/library/ms646272(VS.85).aspx
 # typedef struct tagLASTINPUTINFO {
 #     UINT cbSize;
 #     DWORD dwTime;
 # } LASTINPUTINFO, *PLASTINPUTINFO;
-
 
 class LASTINPUTINFO(ctypes.Structure):
     _fields_ = [
@@ -36,46 +43,122 @@ kernel32 = ctypes.windll.kernel32
 GetTickCount = kernel32.GetTickCount
 Sleep = kernel32.Sleep
 
-
-def wait_until_idle(idle_time=60):
-    '''Wait until no more user activity is detected.
-
-    This function won't return until `idle_time` seconds have elapsed
-    since the last user activity was detected.
-    '''
-
-    idle_time_ms = int(idle_time*1000)
-    liinfo = LASTINPUTINFO()
-    liinfo.cbSize = ctypes.sizeof(liinfo)
-    while True:
-        GetLastInputInfo(ctypes.byref(liinfo))
-        elapsed = GetTickCount() - liinfo.dwTime
-        if elapsed >= idle_time_ms:
-            break
-        Sleep(idle_time_ms - elapsed or 1)
+#
 
 
-def thread_loop():
-    '''Executes continuously (until killed), logging user activity 'chunks'
-    to and output CSV file.
-    '''
+class ActivityMonitorThread(object):
+    # ::Public API::
 
-    alive = True
-    filename = 'blocks_' + time.strftime('%Y%m%d', gmtime())
-    open(runfile, 'w').close()
+    @property
+    def alive(self):
+        return self._alive
 
-    while(alive):
-        block_time = time.strftime('%H:%M:%S', gmtime())
-        block_start = GetTickCount()
-        # Sleep until user idle
-        wait_until_idle()
-        block_end = GetTickCount()
+    @property
+    def chunkfile(self):
+        return self._chunkfile
 
-        with open(filename, 'a') as f:
-            duration_secs = (block_end-block_start)//1000
-            f.write('{}, {}\n'.format(block_time, duration_secs))
+    #
 
-    os.remove(runfile)
+    def __init__(self):
+        self._alive = True
+
+        thread = threading.Thread(target=self._thread_loop, args=())
+        thread.start()
+
+    def _thread_loop(self):
+        '''
+        Executes continuously (until killed), logging user activity 'chunks'
+        to an output CSV file.
+        '''
+        self._finished = False
+        self._chunkfile = '{}blocks_{}.csv'.format(homedir,
+                                                   time.strftime('%Y%m%d',
+                                                                 time.localtime()))
+
+        # Touch a file which will serve as run indicator
+        open(runfile, 'w').close()
+
+        while self._alive:
+            # Activity block start time
+            block_time = time.strftime(TIMEFORMAT, time.localtime())
+            block_start = GetTickCount()
+
+            # Sleep until user idle
+            # TODO Check app death (logout)
+            self._wait_until_idle()
+
+            block_end = GetTickCount()
+
+            with open(self._chunkfile, 'a') as f:
+                duration_secs = (block_end-block_start)//1000
+                f.write('{}, {}\n'.format(block_time, duration_secs))
+
+            # Now wait till there's activity again
+            self._wait_until_active()
+
+        os.remove(runfile)
+        self._finished = True
+
+    def _wait_until_idle(self, idle_time=60):
+        '''
+        Wait until no more user activity is detected.
+
+        This function won't return until 'idle_time' seconds have elapsed
+        since the last user activity was detected.
+        '''
+        idle_time_ms = int(idle_time*1000)
+        liinfo = LASTINPUTINFO()
+        liinfo.cbSize = ctypes.sizeof(liinfo)
+
+        while self._alive:
+            GetLastInputInfo(ctypes.byref(liinfo))
+
+            elapsed = GetTickCount() - liinfo.dwTime
+            if elapsed >= idle_time_ms:
+                break
+
+            Sleep(500)
+
+    def _wait_until_active(self):
+        '''
+        Wait until user activity is detected.
+        '''
+        liinfo = LASTINPUTINFO()
+        liinfo.cbSize = ctypes.sizeof(liinfo)
+
+        while self._alive:
+            GetLastInputInfo(ctypes.byref(liinfo))
+
+            elapsed = GetTickCount() - liinfo.dwTime
+            if elapsed < 1000:
+                break
+
+            Sleep(500)
+
+    def quit(self):
+        self._alive = False
+
+        # Block so that the systray app stays alive while the thread does
+        while not self._finished:
+            pass
+
+
+def tooltip_updater():
+    if os.path.exists(runfile):
+        with open(thread.chunkfile, 'r') as f:
+            timestr = f.readline().split(',')[0]
+
+        startdate = datetime.strptime(timestr, TIMEFORMAT)
+        totalsecs = (datetime.now() - startdate).total_seconds()
+        m, s = divmod(totalsecs, 60)
+        h, m = divmod(m, 60)
+
+        tooltip = 'Tracking: {}:{} today'.format(int(h), int(m))
+        app.update_tooltip(tooltip)
+
+    # Repeat ourselves in a minute
+    if thread.alive:
+        threading.Timer(60, tooltip_updater).start()
 
 
 if __name__ == '__main__':
@@ -85,7 +168,23 @@ if __name__ == '__main__':
 
     if os.path.exists(runfile):
         # Kill current running process
-        raise NotImplementedError()
+        raise ValueError('Already running!')
     else:
-        # Launch thread
-        _thread.start_new_thread(thread_loop, ())
+        try:
+            # Launch thread
+            thread = ActivityMonitorThread()
+            # Start tooltip updater
+            tooltip_updater()
+            # Create systray app
+            app = trayapp.SysTrayApp((),
+                                     'Clock.ico',
+                                     'Tracking time...',
+                                     on_quit=thread.quit) #, default_menu_action_index=1)
+
+        except:
+            thread.quit()
+            app.quit()
+            raise
+
+
+
