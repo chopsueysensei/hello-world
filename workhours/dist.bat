e41b89f3d0c63aec894fd3ef52567473ea3fe044
n00bmind <n00bmindr0b0t@gmail.com>
2017-12-07 09:51:35 +0100

    * Fix incomplete 'python_tools' dependency

--- workhours/dist.bat
+++ workhours/dist.bat
@@ -3,4 +3,8 @@ md dist
 xcopy Clock.ico dist
 xcopy template.mustache dist
 
-cxfreeze workhours.py --target-dir=dist --include-modules=imp --base-name=Win32GUI
+:: Assume 'python_tools' is located two directories up from us
+cxfreeze workhours.py --target-dir=dist --include-path=..\..\ --include-modules=imp,python_tools --base-name=Win32GUI --icon=Clock.ico
+
+if %errorlevel% == 0 goto:eof
+pause
